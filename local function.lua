76fbe7db70a927206f609efbab98d38cc5a713af
SnailSoPretty <166292914+snailsopretty@users.noreply.github.com>
2024-04-11 16:51:20 +0300

    local function.lua

--- local function.lua
+++ local function.lua
@@ -1,70 +1,11 @@
-
---[[
-
-Created By NowThrowYourHandsUp
-
---------------------------------------------
-
-Создаем локальную функцию и приписываем
-ей имя после "local function ..."
-Имя не должно состоять из цифр и букв латинского алфавита,
-а также не должно совпадать с именем уже существующей функции.
-
-Функция заканчиваеться после слова "end"
-Все что после этого слова не относиться к функции.
-
---------------------------------------------
-
-Создаем действие в функции:
-
-local function NameFunction()
+local function NameFunction() -- Создаем локальную функцию, и даем ей название / Create a local function and give it a name
 	
-	"Здесь пишем любое наше действие"
+	print("function active!") -- Создаем наше действие / Create our action
 	
-end
+end -- Конец функции / End of function
 
-Действий может быть несколько.
-
---------------------------------------------
-
-Активируем нашу функцию любым удобным
-способом, это может быть:
-
-wait()
-
-ClickDetector.MouseClick:Connect(function()
-
-ProximityPrompt.Triggered:Connect(function()
-
-Button.MouceButton1Click:Connect(function()
-
-и т.д.
-
---------------------------------------------
-
-Активация функции:
-
-script.Parent.ClickDetector.MouseClick:Connect(function()
+script.Parent.ClickDetector.MouseClick:Connect(function() -- При каком действие наша функция будет воспроизводиться / Under what action will our function be played
 	
-	"Здесь пишем название нашей функции со скобочкоми в конце"
+	NameFunction() -- Применяем нашу функцию / Apply our function
 	
-end)
-
---]] 
-
-
-
-
--- Создаем и активируем нашу функцию:
-
-local function NameFunction()
-	
-	print("function active!")
-	
-end
-
-script.Parent.ClickDetector.MouseClick:Connect(function()
-	
-	NameFunction()
-	
-end)
+end) -- Конец действия / End of Actions
