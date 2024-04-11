2b5a95fe9336c7d02750482781878c07a728e03a
SnailSoPretty <166292914+snailsopretty@users.noreply.github.com>
2024-04-11 17:03:32 +0300

    tween service.lua

--- tween service.lua
+++ tween service.lua
@@ -1,14 +1,14 @@
 
-local TweenService = game:GetService("TweenService") -- Получаем сервер "Tween"
+local TweenService = game:GetService("TweenService") -- Получаем сервер "Tween" / get service "Tween"
 
-local Part = game.Workspace.Part -- Получаем наш объект, в моем случае "Part", объект может быть любой
+local Part = game.Workspace.Part -- Получаем наш объект, в моем случае "Part", объект может быть любой / geting our object
 
-local TweenInfoNew = TweenInfo.new(2) -- Время проведения действия
+local TweenInfoNew = TweenInfo.new(2) -- Время проведения действия / time to action
 
-local Target = {Transparency = 1} -- Само действие
+local Target = {Transparency = 1} -- Само действие / our action
 
-local Active = TweenService:Create(Part, TweenInfoNew, Target) -- Создаем "Tween"
+local Active = TweenService:Create(Part, TweenInfoNew, Target) -- Создаем "Tween" / create "Tween"
 
-wait(5) -- После чего "Tween" будет активирован
+wait(5) -- После чего "Tween" будет активирован / After which "Tween" will be activated
 
-Active:Play() -- Активируем его
+Active:Play() -- Активируем его / Activated it
